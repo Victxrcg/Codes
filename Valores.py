90caca4a0b631a367df8de4414ecf476c6195a3e
Victxrcg <extremosgamer9@gmail.com>
2024-09-24 17:48:57 -0300

    aprendendo ainda

---
+++ Valores.py
@@ -0,0 +1,7 @@
+Valor1=int(input("Digite o valor 1:"))
+Valor2=int(input("Digite o valor 2:"))
+Valor3=int(input("Digite o valor 3:"))
+
+conta= (Valor1 +Valor2) * Valor3
+
+print(f"O valor da soma dos dois primeiros valores multiplicado com o terceiro é de:",conta)
