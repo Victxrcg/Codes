72df0351e57e6180e147f78a75a6a528c04fc34b
Victxrcg <extremosgamer9@gmail.com>
2024-09-24 16:24:25 -0300

    cotação atividade

---
+++ cotação_do_dolar.py
@@ -0,0 +1,6 @@
+real=float(input("Digite o valor em reais:"))
+cotacao=float(input("Digite o valor do dolar atual:"))
+
+cotacao1=real/cotacao
+
+print(f"O valor equivalente ao dolar é de:,{cotacao1:.1f}")
