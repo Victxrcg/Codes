524bed2a0cb4210dd5a0f5a0a635b33194ac41aa
Victxrcg <extremosgamer9@gmail.com>
2024-10-01 18:27:24 -0300

    Atividades e aula sobre listas1

---
+++ lista2.py
@@ -0,0 +1,3 @@
+lista = ["Victor", 15, 15.11, ]
+lista1= ["Aulas do Ederson", 20, "loucura"]
+print(len(lista))
---
+++ lista3.py
@@ -0,0 +1,2 @@
+compras=["maca","pera","arroz",["feijao","bife"]]
+print(compras[1][3])
