7f74cdf24ebc3aa8b951633118789710fc29f964
VictorAntunes <extremosgamer9@gmail.com>
2024-11-08 14:33:37 -0400

    Create atividade6.py
    Atividade6

---
+++ atividade6.py
@@ -0,0 +1,12 @@
+a=int(input("Digite o valor:"))
+b=int(input("Digite o valor:"))
+c=int(input("Digite o valor:"))
+if a>b>c:
+    print(c,b,a)
+if b>c>a :
+    print(a,c,b)
+if c>a>b:
+    print(b,a,c)
+    
+
+    
