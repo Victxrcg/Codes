d5c4bb96538cffd1c1d8be1503c88371e6a06cb9
VictorAntunes <extremosgamer9@gmail.com>
2024-11-08 14:37:25 -0400

    Create numero_de_palavras.py
    Atividade35

---
+++ numero_de_palavras.py
@@ -0,0 +1,4 @@
+nome=input("digite seu nome:")
+nome2=input("digite seu sobrenome:")
+print("O seu nome possui:",len(nome),"caracteres e seu sobrenome possui:",len(nome2) )
+print(nome.split(" "), nome2.split(" "))
