00c261b45c8961d62dff7a3a4cf659c43d307dfd
VictorAntunes <extremosgamer9@gmail.com>
2024-11-08 14:40:22 -0400

    Create aula11.py
    Atividade41

---
+++ aula11.py
@@ -0,0 +1,95 @@
+# Função DEF python 
+
+# def hello():
+#       print('Olá',nome,",Sua senha é:",senha)
+
+# nome=input('digite seu nome:')
+
+# senha=input('digite sua senha:')
+
+# hello(nome,senha)
+
+
+# def cal_pagamento(qnt_horas,valor_horas):
+#     horas= float(qnt_horas)
+#     taxa= float(valor_horas)
+#     if horas <= 40:
+#         salario=horas*taxa
+#     else:
+#         excedida= horas - 40
+#         salario=40*taxa(excedida(1.5*taxa))
+#     print(salario)
+
+# cal_pagamento(35,20)
+
+
+# def inverte(nome,sobrenome):
+#     nomeinverso=sobrenome+','+nome
+#     return nomeinverso
+
+# nome= input('Nome:')
+# sobrenome = input('Sobrenome:')
+
+# invertido=inverte(nome,sobrenome)
+
+# print('Olá',invertido)
+    
+# def par(x):
+#     if (x%2)==0:
+#         return True
+#     else:
+#         return False
+
+# while True:
+#     num = int(input("Insira um Numero:"))
+#     if par(num):
+#         print('é par!')
+#     else:
+#         print('é impar')
+
+# def cadastro():
+#     name = input('Qual seu nome:')
+#     idade = int(input('Idade:'))
+#     return name, idade
+
+# print('Iniciando cadastro...')
+# nome, idade = cadastro()
+# print('cadastro realizado com sucesso!')
+# print('Seu nome é',nome,'e vc tem', idade,'anos de idade')
+
+# def atv(r1,r2,r3):
+#     resultado=((r1+r2)*r3)
+#     return resultado
+
+# r1 = float(input('digite um numero:'))
+# r2 = float(input('digite um numero:'))
+# r3 = float(input('digite um numero:'))
+
+# resultado1=atv(r1,r2,r3)
+# print(resultado1)
+
+# def argumento(senha):
+#    while True:
+
+#         if senha > 0:
+#             print ('P')
+#         elif senha != 0:
+#             print('N')
+#         else:
+#             print('Zero')        
+#         return senha
+
+# escolha = float(input('Digite o valor:'))
+
+# argumento(escolha)
+
+def somaimposto():    
+    taxaimposto == 0.15
+    custo=(input('Digite o custo'))
+    total = custo + taxaimposto
+    print(total)
+
+somaimposto()
+
+
+
