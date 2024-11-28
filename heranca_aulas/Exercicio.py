10d4b77b1f377211aba6f5f5a2c0777b123d551a
VictorAntunes <extremosgamer9@gmail.com>
2024-11-28 20:55:59 -0300

    Atualização do planeta terra

--- heranca_aulas/Exercicio.py
+++ heranca_aulas/Exercicio.py
@@ -2,23 +2,58 @@ class vehicle():
     def __init__(self,mark: str,model:str):
         self.mark = mark
         self.model = model
+
     def fuel_type(self):
-        fuel_type = fuel_type.upper()
-        if ('GASOLINE'!= fuel_type and 'DIESEL' != fuel_type and 'ALCOHOL' != fuel_type):
-            print(fuel_type)
-        else:
-            print('Unkonw value')
+        
+        return 'Unkonw value'
 
     
-    def passangers_capacity(self, passengers: int):
-        if passengers < 0:
-            print('Its not a vehicle')
-        else:
-            print(f'Will bord {passengers} passengers')
+    def passangers_capacity(self):
+        return 0
+        
+class car(vehicle):
 
-test= vehicle('toyota','supra')
-test.fuel_type('gasoline')
-test.passangers_capacity(1)
+    def fuel_type(self):
+        return 'Gasoline/Alcohol'
+    
+    def passangers_capacity(self):
+        return 5
+    
+class motorcycle(vehicle):
 
-# conta = vehicle('toyota','supra')
-# conta.gas_type()
+    def fuel_type(self):
+        return 'Gasoline'
+
+    def passangers_capacity(self):
+        return 2
+
+class truck(vehicle):
+
+    def fuel_type(self):
+        return 'Diesel'
+    
+    def  passangers_capacity(self):
+        return 2
+    
+def system():
+
+    vehicle = [
+        car('toyota','Supra'),
+        motorcycle('Kawasaki','Ninja'),
+        truck('Volvo','H1200'),
+        ]
+     
+    for vehicle in vehicle:
+        print(f'Mark: {vehicle.mark}')
+        print(f'Model: {vehicle.model}')
+        print(f'Fuel type: {vehicle.fuel_type()}')
+        print(f'Passengers capacity: {vehicle.passangers_capacity()}')
+        print('-' * 30)
+        
+        
+
+
+
+
+if __name__ == "__main__":
+    system()
